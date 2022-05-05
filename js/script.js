aa08174e6f849e7e3aecd347de73378c5e92d3ef
Giovanni Ciotta <giociotta@gmail.com>
2022-05-05 16:57:35 +0200

    quasi completo

--- js/script.js
+++ js/script.js
@@ -5,29 +5,31 @@
 const myQuadrato = document.querySelector(".container");
 for (let index = 1; index <= 100; index++) {
     let element;
-    element = `< div class= "tuttinumeri"> ${index} <div/>`;
+    element = `<div class= "tuttinumeri"> ${index} <div/>`;
 
 
+
+       // multipli di 3  e 5 stampo “FizzBuzz” 
+       if (index % 3 == 0 && index % 5 == 0) {
+        element = `<div class= "div.tuttinumeri fizzbuzz"> FizzBuzz <div/>`;
+    }
     // multipli di 3 stampo “Fizz” 
-        if (index % 3 == 0) {
-            element = `< div class= "div.tuttinumeri fizz"> fizz <div/>`;
+        else if (index % 3 == 0) {
+            element = `<div class= "div.tuttinumeri fizz"> fizz <div/>`;
         }
 
 
 
         // multipli di 5 stampo “Buzz” 
         else if (index % 5 == 0) {
-            element = `< div class= "div.tuttinumeri buzz"> Buzz <div/>`;
+            element = `<div class= "div.tuttinumeri buzz"> Buzz <div/>`;
         }
 
 
 
 
 
-        // multipli di 3  e 5 stampo “FizzBuzz” 
-            else if (index % 3 == 0 && index % 5 == 0) {
-                element = `< div class= "div.tuttinumeri fizzbuzz"> FizzBuzz <div/>`;
-            }
+     
     
     
         myQuadrato.innerHTML += element; 
