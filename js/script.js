967f20e93a5286a2de945387e8a6197515521191
Giovanni Ciotta <giociotta@gmail.com>
2022-05-05 16:21:04 +0200

    lista numeri

--- js/script.js
+++ js/script.js
@@ -2,9 +2,34 @@
 // lista numeri da 1 a 100
 
 
-// multipli di 3 stampo “Fizz” 
+const myQuadrato = document.querySelector(".tuttinumeri");
+for (let index = 1; index <= 100; index++) {
+    let element;
+    element = `class = "tuttinumeri" ${index} `;
 
 
-// multipli di 5 stampo “Buzz” 
+    // multipli di 3 stampo “Fizz” 
+        if
 
-// multipli di 3  e 5 stampo “FizzBuzz” 
+
+
+        // multipli di 5 stampo “Buzz” 
+    else if
+
+
+
+
+
+        // multipli di 3  e 5 stampo “FizzBuzz” 
+    
+    
+    
+        myQuadrato.innerHTML += element; 
+    console.log(element);
+}
+
+
+
+
+
+
